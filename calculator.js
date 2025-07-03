96fb33824cf480266ee0f4d020670ed828de5fea
Pruthvi Parmar <ppruthviraj254@gmail.com>
2025-07-03 12:57:27 +0530

    Handles new lines as delimiters

--- calculator.js
+++ calculator.js
@@ -1,16 +1,6 @@
 export function add(numbers) {
+  if (numbers === "") return 0;
 
-    if (numbers === "") return 0;
-    if (!numbers.includes(',')) return parseInt(numbers);
-
-    let numArray = numbers.split(',').map(Number);
-
-    if (numArray.length === 2) {
-        return numArray[0] + numArray[1];
-    }else{
-        numArray = numbers.split(',').map(Number);
-        return numArray.reduce((sum, num) => sum + num, 0);
-    }
-
-    return 0;
+  const numArray = numbers.split(/,|\n/).map(Number);
+  return numArray.reduce((sum, num) => sum + num, 0);
 }
