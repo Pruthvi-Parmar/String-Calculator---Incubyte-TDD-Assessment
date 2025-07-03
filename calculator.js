6b3a861db58875d22fab5e4c925f7573283a5163
Pruthvi Parmar <ppruthviraj254@gmail.com>
2025-07-03 13:10:55 +0530

    Handle  negative numbers and Throws exception listing all negative numbers

--- calculator.js
+++ calculator.js
@@ -10,5 +10,11 @@ export function add(numbers) {
   }
 
   const numArray = numbers.split(delimiter).map(Number);
+  
+  const negatives = numArray.filter(n => n < 0);
+  if (negatives.length > 0) {
+    throw new Error(`negative numbers not allowed ${negatives.join(",")}`);
+  }
+
   return numArray.reduce((sum, num) => sum + num, 0);
 }
