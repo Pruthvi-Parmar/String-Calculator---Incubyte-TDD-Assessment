3178344f57e9e44a5056fedb41359234c59b4cd6
Pruthvi Parmar <ppruthviraj254@gmail.com>
2025-07-03 12:41:37 +0530

    Implemented sum for two comma-separated numbers

--- calculator.js
+++ calculator.js
@@ -1,4 +1,13 @@
 export function add(numbers) {
-  if (numbers === "") return 0;
-  return parseInt(numbers);
+
+    if (numbers === "") return 0;
+    if (!numbers.includes(',')) return parseInt(numbers);
+
+    const numArray = numbers.split(',').map(Number);
+
+    if (numArray.length === 2) {
+        return numArray[0] + numArray[1];
+    }
+
+    return 0;
 }
