11e3085befe2d3e721ad0e4e30fd0862c9d370ad
Pruthvi Parmar <ppruthviraj254@gmail.com>
2025-07-03 13:07:52 +0530

    Added failing test for handling negative numbers

--- calculator.test.js
+++ calculator.test.js
@@ -25,6 +25,9 @@ test('supports custom delimiter', () => {
   expect(add("//;\n1;2")).toBe(3);
 });
 
+test('throws exception for negative numbers', () => {
+  expect(() => add("1,-2,3,-4")).toThrow("negative numbers not allowed -2,-4");
+});
 
 
 });
