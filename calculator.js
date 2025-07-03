3c26773cdb81c330d91bc9b66f8d5d94e71ec9ec
Pruthvi Parmar <ppruthviraj254@gmail.com>
2025-07-03 12:52:34 +0530

    Handles sum of multiple numbers

--- calculator.js
+++ calculator.js
@@ -3,10 +3,13 @@ export function add(numbers) {
     if (numbers === "") return 0;
     if (!numbers.includes(',')) return parseInt(numbers);
 
-    const numArray = numbers.split(',').map(Number);
+    let numArray = numbers.split(',').map(Number);
 
     if (numArray.length === 2) {
         return numArray[0] + numArray[1];
+    }else{
+        numArray = numbers.split(',').map(Number);
+        return numArray.reduce((sum, num) => sum + num, 0);
     }
 
     return 0;
