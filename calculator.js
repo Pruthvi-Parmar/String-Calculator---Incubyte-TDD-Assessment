54944e1dcf53366f13f3c419e5d1dee43ba46f6c
Pruthvi Parmar <ppruthviraj254@gmail.com>
2025-07-08 15:50:53 +0530

    Refactor add function to support range filtering and custom delimiter multiplication; update tests for new functionality

--- calculator.js
+++ calculator.js
@@ -1,20 +1,33 @@
 //To ensure clean and modern code Base, I use ES6 syntax
-export function add(numbers) {
+export function add(numbers, start, end) {
   if (numbers === "") return 0;
 
   let delimiter = /,|\n/;
 
-  if (numbers.startsWith('//')) {
-    const parts = numbers.split('\n');
+  if (numbers.startsWith("//")) {
+    const parts = numbers.split("\n");
     const delimiterPart = parts[0].slice(2);
 
     const matches = [...delimiterPart.matchAll(/\[([^\]]+)\]/g)];
 
     if (matches.length > 0) {
-      const delimiters = matches.map(m => m[1].replace(/[.*+?^${}()|[\]\\]/g, '\\$&'));
-      delimiter = new RegExp(delimiters.join('|'));
+      const delimiters = matches.map((m) =>
+        m[1].replace(/[.*+?^${}()|[\]\\]/g, "\\$&")
+      );
+      delimiter = new RegExp(delimiters.join("|"));
     } else {
-      delimiter = new RegExp(delimiterPart.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'));
+      delimiter = new RegExp(
+        delimiterPart.replace(/[.*+?^${}()|[\]\\]/g, "\\$&")
+      );
+
+      if (delimiterPart == "*") {
+        numbers = parts[1];
+
+        const numArray = numbers.split(delimiter).map(Number);
+        return numArray
+          .filter((n) => n <= 500)
+          .reduce((sum, num) => sum * num, 1);
+      }
     }
 
     numbers = parts[1];
@@ -22,11 +35,19 @@ export function add(numbers) {
 
   const numArray = numbers.split(delimiter).map(Number);
 
-  const negatives = numArray.filter(n => n < 0);
+  const negatives = numArray.filter((n) => n < 0);
   if (negatives.length > 0) {
     console.log(`Negative numbers detected: ${negatives.join(",")}`);
     throw new Error(`negative numbers not allowed ${negatives.join(",")}`);
   }
 
-  return numArray.filter(n => n <= 1000).reduce((sum, num) => sum + num, 0);
+  if (start) {
+    return numArray
+      .filter((n) => n <= 500)
+      .filter((n) => n >= start && n <= end)
+      .reduce((sum, num) => sum + num, 0);
+  } else {
+    return numArray.filter((n) => n <= 500).reduce((sum, num) => sum + num, 0);
+  }
+
 }
